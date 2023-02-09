5252da1615829afe7d812e160a9947a7daeb3820
Kate <katumni4ka@gmail.com>
2023-02-09 12:46:44 +0200

    preparation of models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,38 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     use HasFactory;
+    use Sluggable;
+    public function category()
+    {
+        return $this->hasOne(Category::class);
+    }
+    public function author()
+    {
+        return $this->hasOne(User::class);
+    }
+    public function tags()
+    {
+        return $this->belongsToMany(
+
+            Tag::class,
+            'post_tags',
+            'post_id',
+            'tag_id'
+        );
+    }
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 }
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -2,10 +2,30 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
     use HasFactory;
+    use Sluggable;
+
+    public function posts()
+    {
+        return $this->belongsToMany(
+            Post::class,
+            'post_tags',
+            'tag_id',
+            'post_id'
+        );
+    }
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 }
