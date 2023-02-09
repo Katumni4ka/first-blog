18bcbded52153d3441513935f50d61282331e64a
Kate <katumni4ka@gmail.com>
2023-02-10 00:07:18 +0200

    Setting model Post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,11 +5,16 @@ namespace App\Models;
 use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
     use HasFactory;
     use Sluggable;
+
+    protected $fillable = ['title', 'content'];
+
     public function category()
     {
         return $this->hasOne(Category::class);
@@ -28,12 +33,45 @@ class Post extends Model
             'tag_id'
         );
     }
-    public function sluggable()
+    public function sluggable(): array
     {
         return [
             'slug' => [
                 'source' => 'title'
             ]
         ];
+    }
+
+    public static function add($fields)
+    {
+        $post = new static;
+        $post->fill($fields);
+        $post->user_id = 1;
+        $post->save();
+
+        return $post;
+    }
+
+    public function edit($fields)
+    {
+        $this->fill($fields);
+        $this->save();
+    }
+
+    public function remove()
+    {
+        Storage::delete('uploads/' . $this->image);
+        $this->delete();
+    }
+
+    public function uploadImage($image)
+    {
+        if($image == null) { return; }
+
+        Storage::delete('uploads/' . $this->image);
+        $filename = Str::random(10) . '.' . $image->extention();
+        $image->saveAs('uploads', $filename);
+        $this->image = $filename;
+        $this->save();
     }
 }
