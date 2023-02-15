0c0e661b065519a62093fe13d7e11ddfcc5bf584
Kate <katumni4ka@gmail.com>
2023-02-15 10:45:14 +0200

    Setting model Comment and Subscription

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -9,6 +9,9 @@ class Comment extends Model
 {
     use HasFactory;
 
+    const IS_ALLOW = 1;
+    const IS_DISALLOW = 0;
+
     public function posts()
     {
         return $this->hasOne(Post::class);
@@ -17,5 +20,31 @@ class Comment extends Model
     public function author()
     {
         return $this->hasOne(User::class);
+    }
+
+    public function allow()
+    {
+        $this->status = Comment::IS_ALLOW;
+        $this->save();
+    }
+
+    public function disAllow()
+    {
+        $this->status = Comment::IS_DISALLOW;
+        $this->save();
+    }
+
+    public function toggleStatus()
+    {
+        if($this->status = Comment::IS_DISALLOW)
+        {
+           return $this->allow();
+        }
+        return $this->disAllow();
+    }
+
+    public function remove()
+    {
+        $this->delete();
     }
 }
